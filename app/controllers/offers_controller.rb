b5f5ee65f713e71065b56338ed3b44d86aedd402
Cyrielle Gout <cyriellexg@hotmail.fr>
2021-02-18 16:08:45 +0100

    search bar done

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,6 +1,16 @@
 class OffersController < ApplicationController
   def index
-    @offers = Offer.all
+
+    if params[:query].present?
+      sql_query = " \
+      offers.title @@ :query \
+      OR offers.availabilities @@ :query \
+      OR users.city @@ :query \
+      "
+      @offers = Offer.joins(:user).where(sql_query, query: "%#{params[:query]}%")
+    else
+      @offers = Offer.all
+    end
   end
 
   def show
@@ -23,6 +33,7 @@ class OffersController < ApplicationController
   end
 
   private
+
   def offer_params
     params.require(:offer).permit(:title, :availabilities, :user_id)
   end
