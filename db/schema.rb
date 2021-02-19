2acb7f27dc98eba3a96e47d8093b83f89d4f4cf0
Thomas Moessner <t.moessner04@gmail.com>
2021-02-19 12:08:23 +0100

    premier jet show complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_02_18_133125) do
-
+ActiveRecord::Schema.define(version: 2021_02_18_134955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,8 +52,6 @@ ActiveRecord::Schema.define(version: 2021_02_18_133125) do
     t.integer "destinataire"
     t.bigint "user_id", null: false
     t.bigint "meeting_id", null: false
-
-
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["meeting_id"], name: "index_reviews_on_meeting_id"
